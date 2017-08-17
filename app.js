ceb6fce6a5e60444f1c148c0f50dd7e2b3a7c860
Ryoto Kojima <ryoto.k@gmail.com>
2017-08-17 10:27:09 +0000

    修正

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 'use strict';
-/**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
 function factorial(n) {
     let result = 1;
     for (let i = 1; i <= n; i++){
