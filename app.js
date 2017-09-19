116b061c8e6568753c3b9b040b562570e5928cfd
nomore <sekidopenguin@gmail.com>
2017-09-19 04:48:36 +0000

    jsの実装テスト

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
 * @param {Number} n
 * @returns {Number}
+
 */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+  let result = 1;
+    for(; n > 1; n--) {
+      result *= n;
+    }
     return result;
 }
+
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
