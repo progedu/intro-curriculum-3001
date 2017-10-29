d31983eb94752121f06b83a589e0bfd18018bed7
deboracaffe <yshr.kiyhara@gmail.com>
2017-10-29 12:31:55 +0000

    階上関数の実装

--- app.js
+++ app.js
@@ -7,11 +7,14 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (let i=1; i <= n; i++){
+        result = result * i;
+    }
     return result;
 }
+//アサーションという機能を、オブジェクトとして読み込む記述方法
 const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`); //Template Literalという機能
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
