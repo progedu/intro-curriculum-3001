3bad5d12dd73b7f6077cb4f573033e540cb26f6c
santafe666 <honesty70@gmail.com>
2017-11-23 14:02:04 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
+    if(n > 1){
+        result = factorial(n-1) * n
+    }
     return result;
 }
 const assert = require('assert');
