7a1bc01981baa19762ddfc1830a8bd1423fca8ff
yakan-007 <enomatome@gmail.com>
2018-05-27 01:09:01 +0000

    階乗関数の実装

--- app.js
+++ app.js
@@ -6,13 +6,10 @@
 * @returns {Number}
 */
 function factorial(n) {
-
     let result = 1;
-    
-    for(let i=1; i <= n; i++){
+    for(let i = 1; i <= n; i++){
         result = result * i;
     }
-   
     return result;
 }
 const assert = require('assert');
