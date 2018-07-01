5202f05ec08d4a89f5a50b573023c4ed7fc397e4
silmin <silvestmint@gmail.com>
2018-07-02 05:33:22 +0900

    fix factorial

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    if (n <= 1) return 1;
+    let result = n * factorial(n-1);
     return result;
 }
 const assert = require('assert');
