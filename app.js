4e334178f5249a8d2394771f60017171536986c0
miifriends <miikun114514@gmail.com>
2018-08-23 09:03:02 +0000

    コマンドラインで与えられた引数の階乗を表示できるようにした

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+const number = process.argv[2] || 0; // コマンドラインで与えられた引数。無い場合は 0 を代入
+
 /**
 * 与えられた自然数の階乗を返す
 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
@@ -18,3 +20,5 @@ assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
+
+console.log(factorial(number)); // 階乗を求める関数を実行し、結果をコンソールに表示
