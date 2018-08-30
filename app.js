a3e6b1d76c3589bbfc720fc9e201b1fc722973f1
loveJefdog <42825004+loveJefdog@users.noreply.github.com>
2018-08-30 21:46:42 +0900

    関数の追加

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
         for(let i = 1; i <=n; i++){
-            result = result * i;
+            result = result * i ;
         }
     
     return result;
