5963c353cf0e46e83896f934db158fc7e2bf623b
Kouhei Hamano <marco.fronsys@gmail.com>
2019-02-15 07:42:12 +0000

    階上関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,11 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+     if(n !== result){
+       for (n; n > 1 ; n--){
+         result = result * n
+        }
+    }
     return result;
 }
 const assert = require('assert');
