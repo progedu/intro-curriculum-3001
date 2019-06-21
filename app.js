40b8b730ea26160c9f65d8b0f3ad2f9da297a4a9
shibanyan515 <tai_dm_515@yahoo.co.jp>
2019-06-21 23:03:28 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
     return result;
 }
 const assert = require('assert');
