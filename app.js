b7661c62e311b82e02150cc4e76bf76a94d818b6
Yazrigues <quentropyyy@gmail.com>
2019-10-21 06:35:49 +0000

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,12 +7,20 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+        return result;
 }
 const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+// Node.js が持つアサーションという機能を、オブジェクトとして読み込む記述方法。
+
+assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
+//  ` (バッククオート) で囲まれている文字列は、 ${プログラム内の値} という形式の文字列を
+// 含めることで、変数の値を埋め込むことができる Template Literal という機能。
+
+// 一発合格！ウレシー
