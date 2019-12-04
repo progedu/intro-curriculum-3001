f5de29f9cd5c62a97fc89e25a41ec76374a2db10
haryoiro <mizusecocolte@gmail.com>
2019-12-05 02:35:29 +0900

    solution

--- app.js
+++ app.js
@@ -5,10 +5,11 @@
 * @param {Number} n
 * @returns {Number}
 */
+
 const factorial = n => {    
     let result = 1;
     for(let i = 1;i <= n;i++) {
-        result = i * result++
+        result = i * result
     }
     return result
 }
@@ -19,5 +20,3 @@ assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
-
-//result = result++ * i
