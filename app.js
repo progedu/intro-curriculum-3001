60c277b3968fe48609945ed6d7acc002096eeb73
key-aji-ubuntu <key.developer05@gmail.com>
2020-01-25 14:17:44 +0900

    1以上の自然数の階乗を求める処理を実装しました

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    // const n = process.args[2] || 0;
+    for (let i = 1; i <= n; i ++) {
+        result *= i;
+    }
     return result;
 }
 const assert = require('assert');
