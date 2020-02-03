d6afed336a9d964c934e37b18ff7707b500a4215
Gorilla <log1230gori@outlook.jp>
2020-02-03 15:14:01 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -8,6 +8,18 @@
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
+    for(let i=1;i<=n;i++){
+        /*
+        if(n==1){
+            return result=0;
+        }else if(n==2){
+            return result=2;
+        }else{
+            
+        }
+        */
+       result=result*i;
+    }
     return result;
 }
 const assert = require('assert');
