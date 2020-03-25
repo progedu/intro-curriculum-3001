15fca98c93d9f01201591c7845d78657f7189adc
maacyan <maakuncyan@ybb.ne.jp>
2020-03-25 20:34:56 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,11 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
     if (n===1){
         return result;
     }
-    for(let i=1;i<=n;i++){
+    for(let i=2;i<=n;i++){
         result=result*i;
     }
     return result;
