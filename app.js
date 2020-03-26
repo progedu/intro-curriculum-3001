bc95f9b61fb4f1e717a5257b5b1073f37c994b73
maacyan <maakuncyan@ybb.ne.jp>
2020-03-26 14:33:36 +0900

    assertを追加

--- app.js
+++ app.js
@@ -19,5 +19,6 @@ const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+assert.equal(factorial(4), 24, `4の階乗は24ですが、実際は${factorial(4) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
