03ff09dd65b5a58666feb8458e542d6749e8bc64
cou <courange.c@gmail.com>
2020-03-30 20:58:32 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 'use strict';
-/**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
-function factorial(n) {
-    let result = 1;
-    for(let i = n;i>0;i--){
-    	result *=i;
-    }
-    return result;
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
 }
-const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
-assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
-console.log('すべてのテストを通過しました');
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
