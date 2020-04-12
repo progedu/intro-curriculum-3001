f609892fa62c45095cb49b69b850a58ba19c0976
Ken <kenigawa592@gmail.com>
2020-04-12 13:59:22 +0900

    function factorial(n)の修正

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    
+    for(let i = 1; i <= n; i++){
+        result *= i;
+    }        //1*1, 2*1, 3*2*1, 4*3*2*1
     return result;
 }
 const assert = require('assert');
