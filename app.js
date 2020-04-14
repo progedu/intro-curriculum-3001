2972d9d8b9bedc4c137b976e701cced9fc8208d4
tanukei <tanukeidrum@outlook.jp>
2020-04-14 20:51:10 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -6,11 +6,11 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
-    for (let i = 1; i <= n; i++) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
     result = result * i;
   }
-     return result;
+  return result;
 }
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
