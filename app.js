be62003d8db9e2eedd9dd583a90f2fd3c40cd9e0
AKR1958 <mathtikoa+2019pg@gmail.com>
2020-04-22 15:53:16 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,11 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    
+for (;n > 1 ; n--) {
+    result *= n
+}
+
     return result;
 }
 const assert = require('assert');
