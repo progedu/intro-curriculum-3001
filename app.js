04deec0ed8dc125bf6f04e0bccc78c9fd05c3bf0
Ami Kadota <happy2079@icloud.com>
2020-04-26 16:20:00 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,8 +7,7 @@
 */
 function factorial(n) {
     let result = 1;
-    const number = process.argv[2] || 1;
-    for(i = 1 ;i <= number ;i++){
+    for(let i = 1 ;i <= n ;i++){
         result = result * i ;
     }    
     return result;
