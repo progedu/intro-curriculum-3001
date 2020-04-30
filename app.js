09131b35ba3f3646e8381dc3d3416ae45f7a9953
coco-24 <coco756241@gmail.com>
2020-05-01 07:41:02 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,9 +7,8 @@
 */
 function factorial(n) {
     let result = 1;
-    for (let i = 1; i <= n; i++){
-        result *= i;
-
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
     }
     return result;
 }
