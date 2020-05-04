a48ce8f63135b666a97b7216ee3364ecc73a1170
mkato164 <tkm15gd@gmail.com>
2020-05-04 13:09:02 +0900

    3-1のadd.js修正課題を実装

--- app.js
+++ app.js
@@ -6,13 +6,23 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+    let mul = 1;
+    for (let i = 1; i <= n; i++) {
+    mul = mul * i;
+    // console.log('nは今は'+n);
+    // console.log('iは今は'+i);
+    // console.log('mulは今は'+mul);
+    }
+    return mul;
 }
 const assert = require('assert');
+
+console.log('1について')
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+console.log('2について')
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
+console.log('3について')
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+console.log('10について')
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
