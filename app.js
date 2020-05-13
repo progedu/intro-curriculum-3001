0af528b76bbb5033f64a41218966fdec8a5ced65
tagamiwork <uedawork2020@gmail.com>
2020-05-13 17:05:21 +0900

    会場関数の実装

--- app.js
+++ app.js
@@ -7,12 +7,26 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    // 階乗の計算をする
+    for (let i = n; 0 < i; i--) {
+        result *= i ;
+    } // for
+
     return result;
-}
-const assert = require('assert');
+} // factorial()
+const assert = require('assert'); // Node.js のアサーション機能を、オブジェクトとして読み込む
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
+// test用
+console.log(factorial(4)); 
+
+/*Template Literal という機能
+
+` で囲まれている文字列は、 
+${プログラム内の値} という形式の文字列を含めることで、 
+変数の値を埋め込むことができる 
+
+*/
