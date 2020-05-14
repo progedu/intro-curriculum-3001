b685c9d7480cb52e787108b1fabe9bfc3be5cfd5
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2020-05-14 12:45:30 +0900

    Denoの標準ライブラリに合わせてテストコードを変更した

--- app.js
+++ app.js
@@ -4,15 +4,19 @@
 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
 * @param {Number} n
 * @returns {Number}
+* require('assert) を変更する
+* Import maps を使用してライブラリを管理する
+* maps.json を作成し、インポート対象とURLを管理する
+* deno 実行時にオプション --importmap=maps.json --unstable を付与する
 */
+import { assertEquals } from 'assert';
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
     return result;
 }
-const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
-assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
+assertEquals(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+assertEquals(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
+assertEquals(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+assertEquals(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
