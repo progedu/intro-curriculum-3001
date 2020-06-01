5c24d64dbf0ec0a00d420100ef7f889cc3ab9b94
ryoma hara <ryohr93@gmail.com>
2020-06-01 19:55:03 +0900

    answer of intro-curriculum-3001

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    let i = 1;
+    for (; i <= n; i++) {
+        result = result * i
+    }
     return result;
 }
 const assert = require('assert');
