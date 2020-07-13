db6ea137021de40381fb8ea57e8869232f84f767
nishino ryo <bksn.spm@gmail.com>
2020-07-13 23:30:10 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
 /**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
 function factorial(n) {
     let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+
+    }
     // TODO このコメントを消して正しく実装してください。
     return result;
 }
