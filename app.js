0d19603960a31d00840b9e3c92ab81ab2ef3c17c
ka74mi <ka74mi@icloud.com>
2020-11-23 21:59:16 +0900

    fantorial関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (let f = 1; f <= n; f++) {
+    result = result * f;
+}
     return result;
 }
 const assert = require('assert');
