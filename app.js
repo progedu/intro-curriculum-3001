e3c230bfabb07e21c449797089fefb84040dd6cc
beginnerfuji <teenagefunclub10000@gmail.com>
2020-12-25 09:01:08 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -5,10 +5,13 @@
 * @param {Number} n
 * @returns {Number}
 */
+
+const n = process.argv[2] || 0;
 function factorial(n) {
     let result = 1;
     for (let i = 1; i <= n; i++) {
       result = result * i;
+      console.log(result)
     }
     return result;
   }
