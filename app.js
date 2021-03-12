0627e17f465fd2fda06d6bb4215afdefeceefe8c
pypymi <dr.mizoguchi@gmail.com>
2021-03-12 14:59:36 +0900

    second commit

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
+    let result = 1
+    for (let i=1;i<=n;i++){
+      result *=i;
+    }
     // TODO このコメントを消して正しく実装してください。
     return result;
 }
