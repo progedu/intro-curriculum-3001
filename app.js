e387571697b603760320d73f8b7085d69acc626a
三星太陽 <halvocal555@gmail.com>
2021-03-29 09:31:27 +0900

    ちゃんと計算できるように修正

--- app.js
+++ app.js
@@ -7,8 +7,13 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+    var numder = n;
+    for (let i = 1; i <= numder; i++) {
+        result = result * i;
+    }
+    //console.log(result);
+    n = result;
+    return n;
 }
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
